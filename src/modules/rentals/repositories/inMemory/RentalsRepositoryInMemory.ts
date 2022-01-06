e7f8d90c3da6a569567633247928256629568d51
Guilherme Bento <guilherme1550@hotmail.com>
2022-01-06 14:32:56 -0300

    Create Rental

--- src/modules/rentals/repositories/inMemory/RentalsRepositoryInMemory.ts
+++ src/modules/rentals/repositories/inMemory/RentalsRepositoryInMemory.ts
@@ -1,3 +1,4 @@
+import { ICreateRentalDTO } from "@modules/rentals/dtos/ICreateRentalDTO";
 import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 
 import { IRentalsRepository } from "../IRentalsRepository";
@@ -7,13 +8,37 @@ class RentalsRepositoryInMemory implements IRentalsRepository {
 
   async findOpenRentalByCar(car_id: string): Promise<Rental> {
     return this.rentals.find(
-      (rental) => rental.car_id === car_id && rental.end_date === null
+      // No repositório em memória, a variável rental.end_date não existe, então ao invés de
+      // utilizar null, tem que utilizar !, para o teste funcionar corretamente.
+      (rental) => rental.car_id === car_id && !rental.end_date
     );
   }
+
   async findOpenRentalByUser(user_id: string): Promise<Rental> {
     return this.rentals.find(
-      (rental) => rental.user_id === user_id && rental.end_date === null
+      // No repositório em memória, a variável rental.end_date não existe, então ao invés de
+      // utilizar null, tem que utilizar !, para o teste funcionar corretamente.
+      (rental) => rental.user_id === user_id && !rental.end_date
     );
+  }
+
+  async create({
+    user_id,
+    car_id,
+    expected_return_date,
+  }: ICreateRentalDTO): Promise<Rental> {
+    const rental = new Rental();
+
+    Object.assign(rental, {
+      car_id,
+      user_id,
+      expected_return_date,
+      start_date: new Date(),
+    });
+
+    this.rentals.push(rental);
+
+    return rental;
   }
 }
 
--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,4 +1,8 @@
+import { injectable, inject } from "tsyringe";
+
+import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { AppError } from "@shared/errors/AppError";
 
 interface IRequest {
@@ -7,14 +11,23 @@ interface IRequest {
   expected_return_date: Date;
 }
 
+@injectable()
 class CreateRentalUseCase {
-  constructor(private rentalsRepository: IRentalsRepository) {}
+  constructor(
+    @inject("RentalsRepository")
+    private rentalsRepository: IRentalsRepository,
+    @inject("DayjsDateProvider")
+    private dateProvider: IDateProvider
+  ) {}
 
   async execute({
     car_id,
     user_id,
     expected_return_date,
-  }: IRequest): Promise<void> {
+  }: IRequest): Promise<Rental> {
+    const minimumHour = 24;
+
+    // Não deve ser possível cadastrar um aluguel caso já exista um aberto para o mesmo carro
     const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
       car_id
     );
@@ -23,6 +36,7 @@ class CreateRentalUseCase {
       throw new AppError("Car is unavailable!");
     }
 
+    // Não deve ser possível cadastrar um aluguel caso já exista um aberto para o mesmo usuário
     const rentalOpenToUser = await this.rentalsRepository.findOpenRentalByUser(
       user_id
     );
@@ -30,6 +44,26 @@ class CreateRentalUseCase {
     if (rentalOpenToUser) {
       throw new AppError("There's a rental in progress for user!");
     }
+
+    // O aluguel deve ter duração mínima de 24 horas
+    const dateNow = this.dateProvider.dateNow();
+
+    const compare = this.dateProvider.compareInHours(
+      dateNow,
+      expected_return_date
+    );
+
+    if (compare < minimumHour) {
+      throw new AppError("Invalid return time!");
+    }
+
+    const rental = await this.rentalsRepository.create({
+      car_id,
+      user_id,
+      expected_return_date,
+    });
+
+    return rental;
   }
 }
 
